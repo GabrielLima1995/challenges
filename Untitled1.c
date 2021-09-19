2037dbbe37f9152b49e5a4611a919996da04bf0b
Gabriel Lima <51869362+GabrielLima1995@users.noreply.github.com>
2021-09-19 14:52:10 -0300

    first commit

---
+++ Untitled1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int main ()
+{
+    float m[12][12],soma=0.0;
+    char o;
+    int i,j,n=11,c=0;
+
+    scanf("%c",&o);
+
+    for (i=0;i<12;i++)
+    {
+        for(j=0;j<12;j++)
+        {
+
+          scanf("%f",&m[i][j]);
+        }
+    }
+
+        for (i=1;i<12;i++)
+    {
+        for(j=11;j>=n;j--)
+        {
+          soma+=m[i][j];
+          c++;
+        }
+        n--;
+    }
+
+    if (o == 'S')
+
+    {
+       printf ("%.1f\n",soma);
+
+    }
+
+    if (o == 'M')
+    {
+         printf ("%.1f\n",soma/c);
+
+    }
+
+    return 0;
+}
+
+
