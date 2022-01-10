0d4f92cbad0fbb79696d6487ea7c45e9f52733d9
Gabriel Lima <51869362+GabrielLima1995@users.noreply.github.com>
2022-01-10 20:50:58 -0300

    first commit

---
+++ Untitled1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main ()
+{
+    int t1,t2,t3,t4;
+
+    scanf("%d%d%d%d",&t1,&t2,&t3,&t4);
+
+    t1-=1;
+    t2-=1;
+    t3-=1;
+
+    printf("%d\n",t1+t2+t3+t4);
+
+
+    return 0;
+}
