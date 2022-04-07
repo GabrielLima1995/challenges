e330dd07b10ddb539bdd03d2006a24944fc18ec0
Gabriel Lima <51869362+GabrielLima1995@users.noreply.github.com>
2022-04-06 23:13:19 -0300

    first commit

---
+++ Untitled1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int s,t,f,soma;
+
+    scanf ("%d %d %d",&s,&t,&f);
+
+    soma=s+t+f;
+
+    if (soma >= 24)
+        soma = soma-24;
+    if (soma < 0)
+        soma+=24;
+
+        printf("%d\n",soma);
+
+
+    return 0;
+
+}
